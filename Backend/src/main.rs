534c4a04beb8519762a188824dec5f8a6a68d8f1
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2022-12-21 11:15:58 +0700

    run

--- Backend/src/main.rs
+++ Backend/src/main.rs
@@ -13,3 +13,12 @@ async fn echo(req_body: String) -> impl Responder {
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(hello).service(echo).route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 6476))?
+        .run().await
+}
---
+++ Backend/target/debug/Backend.d
@@ -0,0 +1,1 @@
+/Users/pornpitak.a/Documents/GitHub/Rust-Actix-Api/Backend/target/debug/Backend: /Users/pornpitak.a/Documents/GitHub/Rust-Actix-Api/Backend/src/main.rs
