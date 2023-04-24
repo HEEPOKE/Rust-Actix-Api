91fd6a7a1f8a420a0a7aad9ca85ead9bb3a8e5dd
HEEPOKE <Damon1fx@gmail.com>
2023-04-24 10:29:06 +0700

    :money_with_wings: setup model

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,48 +1,13 @@
 use diesel::prelude::*;
-use super::super::schema::users;
-use super::PgConnection;
-use crate::diesel::RunQueryDsl;
-use crate::diesel::{expression_methods::*, query_dsl::*, result::Error, Insertable, Queryable,QueryResult,OptionalExtension};
-
-#[derive(Queryable, Deserialize, Serialize, Debug, Clone)]
+ 
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i32,
     pub username: String,
     pub email: String,
     pub password: String,
     pub created_at: Timestamp,
-}
-
-#[derive(Insertable, Deserialize, Serialize)]
-#[table_name = "users"]
-pub struct NewUser {
-    pub username: String,
-    pub email: String,
-    pub password: String,
-}
-
-pub fn create_user(conn: &PgConnection, username: String, email: String, password: String) -> User {
-    let newuser = NewUser {
-        username: username,
-        email: email,
-        password: password,
-    };
-
-    diesel::insert_into(users::table)
-        .values(&newuser)
-        .get_result(conn)
-        .expect("Error saving new post")
-}
-
-pub fn get_user(conn: &PgConnection, user_email: String) -> Result<User, String> {
-    use super::super::schema::users::dsl::*;
-    let result:Option<User> = users
-        .filter(email.eq(user_email))
-        .first::<User>(conn).optional()
-        .expect("Invalid Email Provided");
-    if result.is_some(){
-        return Ok(result.unwrap())
-    }else{
-        return Err("Invalid Email Id".to_string())
-    }
+    pub updated_at: Timestamp,
 }
--- src/schema.rs
+++ src/schema.rs
@@ -1,11 +1,13 @@
 // @generated automatically by Diesel CLI.
 
-table! {
+diesel::table! {
     users (id) {
         id -> i32,
         username -> Varchar,
         email -> Varchar,
         password -> Varchar,
+        tel -> Varchar,
         created_at -> Timestamp,
+        update_at -> Timestamp,
     }
 }
